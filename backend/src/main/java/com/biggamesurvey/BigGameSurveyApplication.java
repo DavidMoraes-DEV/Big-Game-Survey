9e013215a47e5a8b7886f5577e1b7cdbce75fe86
DavidMoraes-DEV <investtraderrs@gmail.com>
2023-01-12 15:28:42 -0300

    Rename main folder and new configs

--- backend/src/main/java/com/biggamesurvey/BigGameSurveyApplication.java
+++ backend/src/main/java/com/biggamesurvey/BigGameSurveyApplication.java
@@ -1,4 +1,4 @@
-package com.microserviceshrworker;
+package com.biggamesurvey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
