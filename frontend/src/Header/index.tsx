f84f04babd24a89f81a9ba146c75d22e1e0c54f1
DavidMoraes-DEV <investtraderrs@gmail.com>
2023-01-21 22:41:04 -0300

    Add Header Component

---
+++ frontend/src/Header/index.tsx
@@ -0,0 +1,16 @@
+import MainLogo from "../assets/images/logo.svg";
+import "./styles.css";
+
+const Header = () => {
+  return (
+    <header className="main-header">
+      <img src={MainLogo} alt="Logo-Principal" />
+      <div className="logo-text">
+        <span className="logo-text-1">Big Game</span>
+        <span className="logo-text-2"> Survey</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
