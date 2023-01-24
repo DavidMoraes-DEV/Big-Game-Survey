952c62fa7a3824c878a0d1eb4a679cd8057377d2
DavidMoraes-DEV <investtraderrs@gmail.com>
2023-01-23 22:15:12 -0300

    Basic Layout Records

--- frontend/src/pages/Records/index.tsx
+++ frontend/src/pages/Records/index.tsx
@@ -1,9 +1,64 @@
-import './styles.css';
+import "./styles.css";
 
 const Records = () => {
-    return (
-        <h1>Records</h1>
-    );
-}
+  return (
+    <div className="page-container">
+      <table className="records-table" cellPadding="0" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>INSTANTE</th>
+            <th>NOME</th>
+            <th>IDADE</th>
+            <th>PLATAFORMA</th>
+            <th>GÊNERO</th>
+            <th>TÍTULO DO GAME</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>20/01/2023 20:30</td>
+            <td>Washington Soares</td>
+            <td>25</td>
+            <td>XBOX</td>
+            <td>Ação - Aventura</td>
+            <td>The last of US</td>
+          </tr>
+          <tr>
+            <td>20/01/2023 20:30</td>
+            <td>Washington Soares</td>
+            <td>25</td>
+            <td>XBOX</td>
+            <td>Ação - Aventura</td>
+            <td>The last of US</td>
+          </tr>
+          <tr>
+            <td>20/01/2023 20:30</td>
+            <td>Washington Soares</td>
+            <td>25</td>
+            <td>XBOX</td>
+            <td>Ação - Aventura</td>
+            <td>The last of US</td>
+          </tr>
+          <tr>
+            <td>20/01/2023 20:30</td>
+            <td>Washington Soares</td>
+            <td>25</td>
+            <td>XBOX</td>
+            <td>Ação - Aventura</td>
+            <td>The last of US</td>
+          </tr>
+          <tr>
+            <td>20/01/2023 20:30</td>
+            <td>Washington Soares</td>
+            <td>25</td>
+            <td>XBOX</td>
+            <td>Ação - Aventura</td>
+            <td>The last of US</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Records;
