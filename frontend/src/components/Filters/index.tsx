04599e2b51c5d138a223b0ded7d76f7f53d8ab2d
DavidMoraes-DEV <investtraderrs@gmail.com>
2023-01-27 12:25:49 -0300

    Update Filters Component

--- frontend/src/components/Filters/index.tsx
+++ frontend/src/components/Filters/index.tsx
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 import "./styles.css";
 
 type Props = {
-    link: string;
-    linkText: string;
-}
+  link: string;
+  linkText: string;
+};
 
 const Filters = ({ link, linkText }: Props) => {
   return (
     <div className="filters-container records-actions">
-      <Link to={link} />
-      <button className="action-filters">{linkText}</button>
+      <Link to={link}>
+        <button className="action-filters">{linkText}</button>
+      </Link>
     </div>
   );
 };
