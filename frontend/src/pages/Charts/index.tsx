e6c56aa0b48155871da6d9deb2d1e0d59d999339
DavidMoraes-DEV <investtraderrs@gmail.com>
2023-05-09 17:52:36 -0300

    Add page chart integrated

--- frontend/src/pages/Charts/index.tsx
+++ frontend/src/pages/Charts/index.tsx
@@ -1,22 +1,105 @@
-import Filters from '../../components/Filters';
-import './styles.css';
+import Filters from "../../components/Filters";
+import Chart from "react-apexcharts";
+import { barOptions, pieOptions } from "./chart-options";
+import "./styles.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  buildBarSeries,
+  getGenderChartData,
+  getPlatformChartData,
+} from "./helpers";
+
+type PieChartData = {
+  labels: string[];
+  series: number[];
+};
+
+type BarChartData = {
+  x: string;
+  y: number;
+};
+
+const initialPieData = {
+  labels: [],
+  series: [],
+};
+
+const BASE_URL = "http://localhost:8080";
 
 const Charts = () => {
-    return (
-        <div className='page-container'>
-            <Filters  link='/records' linkText='VER TABELA'/>
-            <div className='chart-container'>
-                <div className='top-related'>
-                    <h1 className='top-related-title'>
-                        Jogos mais votados
-                    </h1>
-                    <div className='games-container'>
-                        <h1>chart1</h1>
-                    </div>
-                </div>
-            </div>
+  const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
+  const [platformData, setPlataformData] =
+    useState<PieChartData>(initialPieData);
+  const [genderData, setGenderData] = useState<PieChartData>(initialPieData);
+
+  useEffect(() => {
+    async function getData() {
+      const recordsResponse = await axios.get(`${BASE_URL}/records`);
+      const gamesResponse = await axios.get(`${BASE_URL}/games`);
+
+      const barData = buildBarSeries(
+        gamesResponse.data,
+        recordsResponse.data.content
+      );
+      setBarChartData(barData);
+
+      const platformChartData = getPlatformChartData(
+        recordsResponse.data.content
+      );
+      setPlataformData(platformChartData);
+
+      const genderChartData = getGenderChartData(recordsResponse.data.content);
+      setGenderData(genderChartData);
+    }
+    getData();
+  }, []);
+
+  return (
+    <div className="page-container">
+      <Filters link="/records" linkText="VER TABELA" />
+      <div className="chart-container">
+        <div className="top-related">
+          <h1 className="top-related-title">Jogos mais votados</h1>
+          <div className="games-container">
+            <Chart
+              options={barOptions}
+              type="bar"
+              width="700"
+              height="650"
+              series={[{ data: barChartData }]}
+            />
+          </div>
         </div>
-    );
-}
+        <div className="charts">
+          <div className="platform-chart">
+            <h2 className="chart-title">Plataformas</h2>
+            <Chart
+              options={{
+                ...pieOptions,
+                labels: platformData?.labels,
+              }}
+              type="donut"
+              series={platformData?.series}
+              width="350"
+            />
+          </div>
+          <div className="gender-chart">
+            <h2 className="chart-title">Gêneros</h2>
+            <Chart
+              options={{
+                ...pieOptions,
+                labels: genderData?.labels,
+              }}
+              type="donut"
+              series={genderData?.series}
+              width="350"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Charts;
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -20,8 +20,3 @@ export type Game = {
     title: string;
     platform: Platform;
 }
-
-export type ChartItem = {
-    x: string;
-    y: number;
-}
